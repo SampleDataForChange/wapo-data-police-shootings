65f9a69e3f30f08e75ca9ee49aead06fdfabbb1d
John McCall <John@lowlydba.com>
2020-09-05 10:16:09 -0400

    revert sex to gender

--- build/mssql/populate.sql
+++ build/mssql/populate.sql
@@ -3,7 +3,7 @@ EXEC dbo.sp_ReloadCity;
 EXEC dbo.sp_ReloadFlee;
 EXEC dbo.sp_ReloadMannerOfDeath;
 EXEC dbo.sp_ReloadRace;
-EXEC dbo.sp_ReloadSex;
+EXEC dbo.sp_ReloadGender;
 EXEC dbo.sp_ReloadStates;
 EXEC dbo.sp_ReloadThreatLevel;
 EXEC dbo.sp_ReloadArmed;
